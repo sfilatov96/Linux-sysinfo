e3f0af5bcfaa4739b816760cd69f29203bba0b0a
sfilatov96 <stepa-filatov@yandex.ru>
2016-12-18 17:55:07 +0300

    mod

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get install apache2
 apt-get install nginx
-a2enmode cgi
+a2enmod cgi
 cp index.py /var/www/index.py
 cp apache-sysinfo.conf /etc/apache2/sites-enabled/apache-sysinfo.conf
 cp nginx-sysinfo.conf /etc/nginx/sites-availible/nginx-sysinfo.conf
