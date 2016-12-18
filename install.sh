8b933c47ca7fdffd10a8a3654b2210f4da0bc009
sfilatov96 <stepa-filatov@yandex.ru>
2016-12-18 17:49:57 +0300

    install added

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo su
+apt-get install apache2
+apt-get install nginx
+a2enmode cgi
+cp index.py /var/www/index.py
+cp apache-sysinfo.conf /etc/apache2/sites-enabled/apache-sysinfo.conf
+cp nginx-sysinfo.conf /etc/nginx/sites-availible/nginx-sysinfo.conf
+service nginx restart
+a2ensite apache-sysinfo
+service apache2 reload
+service apache2 restart
+crontab cron.bak
