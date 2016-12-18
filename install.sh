47ec5e921cb96c41ea3cbe5064525e8d1d822c38
sfilatov96 <stepa-filatov@yandex.ru>
2016-12-18 18:36:57 +0300

    chmod

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apt-get install apache2
 apt-get install nginx
 a2enmod cgi
 cp index.py /var/www/index.py
+chmod 777 /var/www/index.py
 cp apache2-sysinfo.conf /etc/apache2/sites-available/apache2-sysinfo.conf
 cp nginx-sysinfo.conf /etc/nginx/sites-enabled/nginx-sysinfo.conf
 service nginx restart
