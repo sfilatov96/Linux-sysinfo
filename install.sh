e4fe21a102690877e08a105ff8a69a515f6e5db2
sfilatov96 <stepa-filatov@yandex.ru>
2016-12-19 20:49:02 +0300

    last

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt-get update
-apt-get install apache2
-apt-get install nginx
-apt-get install sysstat
+apt-get -y install apache2
+apt-get -y install nginx
+apt-get -y install sysstat
 a2enmod cgi
 cp index.py /var/www/index.py
 cp index.py /var/www/html/index.py
@@ -9,7 +9,7 @@ chmod 777 /var/www/index.py
 chmod 777 /var/www/html/index.py
 cp apache2-sysinfo.conf /etc/apache2/sites-available/apache2-sysinfo.conf
 rm /etc/nginx/sites-enabled/default
-cp nginx-sysinfo.conf > /etc/nginx/sites-enabled/default
+cp nginx-sysinfo.conf  /etc/nginx/sites-enabled/default
 cat ports.conf > /etc/apache2/ports.conf 
 service nginx restart
 a2ensite apache2-sysinfo
