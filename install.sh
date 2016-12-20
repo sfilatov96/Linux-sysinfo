a0f6fd9fd7330b0790eac4c0cf68938c5550fee8
sfilatov96 <stepa-filatov@yandex.ru>
2016-12-20 12:54:17 +0300

    rgsddffefsd

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get -y install apache2
 apt-get -y install nginx
 apt-get -y install sysstat
+apt-get install python-minimal
 a2enmod cgi
 cp index.py /var/www/html/index.py
 chmod +x /var/www/html/index.py
@@ -14,4 +15,5 @@ service apache2 reload
 service apache2 restart
 crontab cron.bak
 service nginx restart
+touch /var/log/mpstat.log  /var/log/iostat.log  /var/log/tcp_conn.log /var/log/udp_conn.log /var/log/inodes.log /var/log/disk_spaces.log /var/log/network_loading.log
 
